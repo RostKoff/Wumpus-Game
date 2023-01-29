226dd315465ed7dd3bc8dfedc77b04b81fb3473d
Rostyslav Kovalenko <roskovalua@gmail.com>
2023-01-29 13:15:26 +0200

    Menu added + Game function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-const short int cave[20][3] = {{1, 7, 4}, {2, 9, 0}, {3, 11, 1}, {4, 13, 2}, {0, 5, 3},
+const int cave[20][3] = {{1, 7, 4}, {2, 9, 0}, {3, 11, 1}, {4, 13, 2}, {0, 5, 3},
                   {14, 4, 6}, {7, 16, 5}, {6, 0, 8}, {9, 17, 7}, {8, 1, 10},
                   {11, 18, 9}, {10, 2, 12}, {13, 19, 11}, {12, 3, 14}, {5, 15, 13},
                   {19, 14, 16}, {15, 6, 17}, {16, 8, 18}, {17, 10, 19}, {18, 12, 15}};
@@ -9,53 +9,46 @@ bool connectedRooms(int room, int element);
 
 class entity {
     public:
-        void setCurrentPos(short int pos);\
-        short int getCurrentPos();
+        void setCurrentPos(int pos);\
+        int getCurrentPos();
     protected:
-        short int current_position = 0;
+        int currentPosition = 0;
 };
 
-void entity::setCurrentPos(short int pos) {
-    current_position = pos;
+void entity::setCurrentPos(int pos) {
+    currentPosition = pos;
 }
 
-short int entity::getCurrentPos() {
-    return current_position;
+int entity::getCurrentPos() {
+    return currentPosition;
 }
 
 class Player: public entity {
     public:
-        void move(int new_pos);
-        void shoot(int pos);
-        short int getArrows();
+        void move(int newPos);
+        void shoot();
+        int getArrowsNum();
     private:
-        short int num_of_arrows = 3;
+        int numOfArrows = 3;
 };
 
-void Player::move(int new_pos) {
-    if(connectedRooms(current_position, new_pos)) current_position = new_pos;
+void Player::move(int newPos) {
+    currentPosition = newPos;
 }
 
-
-void Player::shoot(int pos) {
-    if(connectedRooms(current_position, pos)) {
-        std::cout<<"Arrow in "<<pos<<"\n";
-        num_of_arrows--;
-    }
-    else std::cout<<"Wrong room\n";
+void Player::shoot() {
+    numOfArrows--;
 }
 
-short int Player::getArrows() {
-    return num_of_arrows;
+int Player::getArrowsNum() {
+    return numOfArrows;
 }
 
-void chooseOption(Player* player);
+void menu();
+void game();
 
 int main() {
-    Player player;
-    while (1) {
-        chooseOption(&player);   
-    }
+    menu();
     return 0;
 }
 
@@ -65,33 +58,78 @@ bool connectedRooms(int room, int element) {
     return 0;
 }
 
-void chooseOption(Player* player) {
-    char option;
-    int room;
-    std::cout<<"You are in room "<<player->getCurrentPos();
-    std::cout<<"\nArrows left: "<<player->getArrows();
-    std::cout<<"\nTunnels lead to: ";
-    for(int i=0; i<3; i++) {
-        std::cout<<cave[player->getCurrentPos()][i]<<" ";
+void menu() {
+    while (1) {
+        char option;
+        std::cout<<"[1] Start game\n[2] Instructions\n[q] Quit\nOption?: ";
+        std::cin>>option;
+        if(option == 'q') break;
+        switch (option) {
+        case '1':
+            game();
+            break;
+        case '2':
+            std::cout<<"Hello guys!\nPress any key to continue...\n";
+            _getwch();
+            break;  
+        default:
+            std::cout<<"Wrong option!\n";
+            break;
+        }
     }
-    std::cout<<"\nShoot or move[s/m]?: ";
-    std::cin>>option;
     
-    switch (option) {
-    case 'm':
-        std::cout<<"Where to?: ";
-        std::cin>>room;
-        player->move(room);
-        break;
-    case 's':
-        std::cout<<"Where to?: ";
-        std::cin>>room;
-        player->shoot(room);
-        break;
-    default:
-        std::cout<<"Wrong option!\n";
-        break;
-    }
-    return;
 }
 
+void game() {
+    Player player;
+    char option;
+    int selectedRoom;
+    bool gameOver = 0;
+
+    std::cout<<"Note: You can always quit with typing [q]\n";
+    while(!gameOver) {
+        std::cout<<"\nYou are in room "<<player.getCurrentPos();
+        std::cout<<"\n\tArrows left: "<<player.getArrowsNum();
+        std::cout<<"\nTunnels lead to: ";
+        for(int i=0; i<3; i++) {
+            std::cout<<cave[player.getCurrentPos()][i]<<" ";
+        }
+        std::cout<<"\nShoot or move[s/m]?: ";
+
+        std::cin>>option;
+        
+        switch (option) {
+        case 'm':
+            std::cout<<"Where to?: ";
+            std::cin>>selectedRoom;
+            if(connectedRooms(player.getCurrentPos(), selectedRoom)) 
+                player.move(selectedRoom);
+            break;
+        case 's':
+            std::cout<<"Where to?: ";
+            std::cin>>selectedRoom;
+            
+            if(connectedRooms(player.getCurrentPos(), selectedRoom)) { 
+                std::cout<<"Arrow in "<<selectedRoom<<"\n";
+                player.shoot();
+
+                if(player.getArrowsNum() == 0) {
+                    std::cout<<"Out of Arrows!\nYou lose!\nPress any key to continue...\n";
+                    _getwch();
+                    gameOver = 1;
+                }
+                        
+            }
+            else 
+                std::cout<<"Wrong room\n";
+            break;
+        case 'q':
+            gameOver = 1;
+            break;
+        default:
+            std::cin.ignore(1000, '\n');
+            std::cout<<"Wrong option!\n";
+            break;
+        }
+    }
+}
